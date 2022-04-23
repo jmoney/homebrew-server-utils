9a6987305f7e302a2d6fc61c9d6a1aee3e32d1fa
goreleaserbot <noreply@gmail.com>
2022-04-23 00:15:02 -0500

    Brew formula update for server-tls-monitor version v0.0.2

--- Formula/server-tls-monitor.rb
+++ Formula/server-tls-monitor.rb
@@ -5,13 +5,13 @@
 class ServerTlsMonitor < Formula
   desc "Reports tls certificate metrics"
   homepage "https://jmoney.dev/"
-  version "0.0.1"
+  version "0.0.2"
   license "Apache 2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jmoney/server-tls-monitor/releases/download/v0.0.1/server-tls-monitor_0.0.1_Darwin_arm64.tar.gz"
-      sha256 "7b78cfdb2e6cf0d2998cc242e9a29295fa27c7ee7fbafcbec46495e782e39aa6"
+      url "https://github.com/jmoney/server-tls-monitor/releases/download/v0.0.2/server-tls-monitor_0.0.2_Darwin_arm64.tar.gz"
+      sha256 "5e30b8a395a0ba3bc386bf6f70488e33e54faa88f3182a7d010385f42d44327b"
 
       def install
         bin.install "server-tls-monitor"
@@ -21,8 +21,8 @@ class ServerTlsMonitor < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/jmoney/server-tls-monitor/releases/download/v0.0.1/server-tls-monitor_0.0.1_Linux_arm64.tar.gz"
-      sha256 "d10c5b3f891055fd4219e926554a5c50307cbcc67b9dfc385ecc8ada13b097c4"
+      url "https://github.com/jmoney/server-tls-monitor/releases/download/v0.0.2/server-tls-monitor_0.0.2_Linux_arm64.tar.gz"
+      sha256 "fe7b28d22839b34c18f009523dd3f8a19a889e85f1a07dbda2819126c7c2de69"
 
       def install
         bin.install "server-tls-monitor"
@@ -32,6 +32,5 @@ class ServerTlsMonitor < Formula
 
   test do
     system "#{bin}/server-tls-monitor -h"
-    ...
   end
 end

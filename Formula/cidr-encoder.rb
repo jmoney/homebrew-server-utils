62ca54eb617eec9a522dd1fefb79a3ff3d636dd5
goreleaserbot <noreply@gmail.com>
2024-06-12 21:19:59 -0500

    Brew formula update for cidr-encoder version v0.5

--- Formula/cidr-encoder.rb
+++ Formula/cidr-encoder.rb
@@ -5,13 +5,13 @@
 class CidrEncoder < Formula
   desc "CLI for time efficiently encoding cidr blocks to find if an IP exists in a range"
   homepage "https://jmoney.dev/cidr-encoder"
-  version "0.4"
+  version "0.5"
   license "Apache 2.0"
   depends_on :macos
 
   on_arm do
-    url "https://github.com/jmoney/cidr-encoder/releases/download/v0.4/tokenizer_cli_0.4_darwin_arm64.tar.gz"
-    sha256 "57a0d323e93b01209b7f3ff086dbb104157710b262cacbac26357e89b1aedc94"
+    url "https://github.com/jmoney/cidr-encoder/releases/download/v0.5/tokenizer_cli_0.5_darwin_arm64.tar.gz"
+    sha256 "be02c68146d6d22db27de96e1d96953fe1a8ee5d532e42d712e44d9d67cd0a0b"
 
     def install
       bin.install "cidr-encoder"
